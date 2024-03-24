5d3a73e8a7b9e9efa97d8bc172cffca4e448a1ef
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-03-24 16:46:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,6 +27,55 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+}
+
+resource "aws_eip" "my_eip" {
+  vpc = true
+}
+
+resource "aws_route_table" "private_route_table" {
+  count = length(aws_subnet.private_subnet)
+
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Private Route Table ${count.index}"
+  }
+}
+
+resource "aws_route" "private_subnet_nat_route" {
+  count            = length(aws_subnet.private_subnet)
+  route_table_id   = aws_route_table.private_route_table[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id  = aws_nat_gateway.my_nat_gateway.id
+}
+
 
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks-cluster-sg"
