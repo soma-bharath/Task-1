552ed778973116c95d275ad211a8e5d05d9437ca
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-03-24 19:36:18 +0530

    Update ECR.tf

--- ECR.tf
+++ ECR.tf
@@ -1,5 +1,4 @@
 
 resource "aws_ecr_repository" "my_repository" {
-  name = "my-ecr-repository" # Specify the name for your ECR repository
-  # Optionally, you can specify additional configuration options here
+  name = "my-ecr-repository"
 }
