372a73a02a7f0a6a168a54c60dab88358946e495
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-03-24 15:46:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,3 +11,15 @@ provider "aws" {
   region     = "us-west-2"
 
 }
+
+resource "null_resource" "argo_install" {
+  provisioner "local-exec" {
+    command = <<EOT
+      sudo chmod 777 kubesetup.sh
+      sudo bash ${path.module}/kubesetup.sh
+    EOT
+
+    #working_dir = "/tmp"  # You can change the working directory as needed
+  }
+depends_on=[aws_eks_cluster.testekscluster,aws_eks_node_group.testeksclusternode]
+}
