65033bc16abc672f59768091ca5e14d86d98748b
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-03-24 19:18:50 +0530

    Update aws_glue.tf

--- aws_glue.tf
+++ aws_glue.tf
@@ -1,25 +1,26 @@
+resource "aws_glue_catalog_database" "example"{
+name= "example"
+}
 
-resource "aws_glue_job" "example_job" {
-  name     = "example_glue_job"
+resource "aws_glue_job" "example" {
+  name     = "example"
   role_arn = aws_iam_role.glue_role.arn
   command {
     name            = "glueetl"
-    script_location = "s3://your-bucket-name/path/to/script.py" # Change this to your script location
+    script_location = "s3://ekss3bucket102989/one.py" # Change this to your script location
   }
-  default_arguments = {
-    "--job-language" = "python"
-  }
+depends_on = [aws_iam_role_policy_attachment.glue_policy_attachment]
 }
 
+resource "aws_glue_workflow" "example"{
+name= "example"
+}
 # Create Glue Trigger as Schedule
-resource "aws_glue_trigger" "example_trigger" {
+resource "aws_glue_trigger" "example" {
   name          = "example_glue_trigger"
-  type          = "SCHEDULED"
-  workflow_name = aws_glue_job.example_job.name
-
-  schedule = "cron(0 0 * * ? *)" # Schedule to run daily at midnight
-
+  type          = "ON_DEMAND"
+  workflow_name = aws_glue_job.example.name
   actions {
-    job_name = aws_glue_job.example_job.name
+    job_name = aws_glue_job.example.name
   }
 }
